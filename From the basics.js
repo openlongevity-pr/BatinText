2ee29f75d00ce1cad51952e666701c9da6d60cd7
openlongevity-pr <107894811+openlongevity-pr@users.noreply.github.com>
2022-06-22 01:37:12 +0300

    Add files via upload

---
+++ From the basics.js
@@ -0,0 +1,109 @@
+From the basics.
+
+I often hear from many a request, wish or even a demand to please start talking about longevity from the very beginning.  From the basics.
+
+That’s a fair ask. From one side, I believe I have said everything on the topic, but from the other side – it’s never difficult to repeat the essentials. So, here is my viewpoint.
+
+I postulate on the moral axiom: it is better to be alive than to be dead. It is better to be than not to be. That is no question to me.
+
+Now I want to share a stunning fact. Fifty percent of people will not agree with this statement. Maybe even more than 50%.
+
+Usually, I refrain from debating with people who see no point in continuing life. It is their life, and they can do with it whatever they please to do. If it were not for one BUT...
+
+Lives of people who want to live are in the hands of people who choose death. This setup is due to politics, budgets, and the public opinion.
+
+When someone buys a ticket to attend a sport event, they become a soccer lobbyist.  This person signals to society: ”hey, soccer is important”. A million of such signals result in constructing a stadium. The same is true of the rest in life: chewing gum, running shoes, and the like – people voice their interests which results in a pronounced vector of public interest. The wheels of the economy are spinning, and society goes – somewhere.
+
+If your interest is not in longevity – you go and lead others towards death.
+
+There is no evil will in it – it happens so for the sole reason that the road to longevity goes in another direction.
+
+We always make choices, be them private or public. If you attended a soccer game, you missed javelin throwing. If everybody went to a Cup Winners' Cup game, nobody came to watch javelin, hammer or shot throwing. Javelin and hammer athletes are depressed. They have a lower budget, and there are less people willing to have sex with shot throwers. The evolution starts working against throwing javelin, hammer, shot.
+
+That’s exactly the reason why there is no 100-storey Longevity Institute – anywhere in the world. Its bricks were used to build shopping malls, amusement parks and other establishments.
+
+There are 3-storey aging research centers in the US, Germany and Australia, but they have not yet discovered a cure for old age. Probably due to the fact that 97 storeys are not supported by the public interest.
+
+Why do people disagree with the idea “it is better to be alive than to be dead”?
+
+First of all, due to the fact that people like to argue, and this idea is too simple and evident. Maybe people think that they appear smarter when they start speculating: “well, you know, 2 plus 2 does not always equal 4”. People tend to argue with Facebook posts, in forums, with a point of view they have just heard – but they are reluctant to argue with a book. A book remains the most reputable source of knowledge. There are of course academic publications – however they remain inaccessible for most people, as they require specialized background or knowledge to read. That is why we do hope that a well-written book may initiate changes in society. Das Kapital (aka Capital) is a good example.
+
+The second obstacle on the way to accept the idea “it is better to be alive than to be dead” is a mystic, religious, esoteric picture of the world.  Simply put, belief in the afterlife. For religious people there is no death as non-existence. There is rather a passage, most probably to a better life. Almost everyone thinks that they are good and have a higher chance to get to heaven. Moreover, the magical creator of the Universe is expected to be kind-hearted, and it is hard to imagine that he will condemn someone to endless suffering, right?
+
+It makes no sense to search for logic talking to people who believe in the afterlife. This very concept emerged as a response to the awareness of death. It is a kind of psychotherapy aimed at reconciling a person with thoughts about death, so that they can calmly reproduce, raise kids – without thinking of negative things and going crazy from the thought that they will die and all is in vain.
+
+The concept of death denial makes one’s life easier, but at the same time the fight against death becomes meaningless.
+
+Why should one postpone meeting with God?
+
+Death is not only denied, it is also justified as care for future generations. Like the belief that old people get in the way of innovations. It is a fascist position, put bluntly. In general, people spend a lot of energy wishing death to bad people (bad from their point of view). Also if neuro renewal advanced, people may be less set in their ways…
+
+At some point their own grandchildren start to wait for their death to inherit their homes.
+
+Our death is economically profitable, too. It is hard to imagine a pension fund that has collected the retirees’ money and invest it in extending their lives. What if it works out? Then the pension fund will go broke. It is not in its business model.
+
+Note an interesting fact: Pension funds are also managed by people, and these very people would definitely benefit from getting a cure from aging – for themselves. However, the public interest gets in the way of their desire to live.
+
+Isn’t it amazing: people have themselves given their money to those who are economically interested in their death? Well, not exactly themselves. The government did it. The government that also views the “silver tsunami” as a problem and not as an achievement. After all, the growing amount of elderly people is labeled “tsunami” and not “happiness”. Coincidence? Not really.
+
+Changing the concept of the world order is a very energy consuming action for the brain. And it is exactly this action that establishes the idea that “it is better to be alive than to be dead”.
+
+It is an ethical concept to the brain: to rank everything on a scale “movement to life or to death”. Besides, the brain feels that if it accepts this seemingly innocent idea, the next step will be a suggestion to do something, which puts at danger one’s established lifestyle with its dopamine reward. No way!
+
+Maybe this is the biggest barrier on the way to immortality: people are concentrated with endless enjoyment. Having lunch, chatted, buying something, selling something, enjoying oneself, scratching one’s back, confirming one’s social status, going to bed. We tend to do what we are used to do – and get pleasure from doing so. There is little pleasure in the fight with death. Why do we only start fighting the concept of death when it is too near to do anything about?
+
+People are not at all indifferent to their own death and very quickly forget about all benefits of the afterlife if their life is in danger. They swim, extinguish fires, take chemotherapy, and betray – to remain alive. Very few will start saying that death is not necessarily bad, if you may die soon due to inaction.
+
+The more time separates a person from their possible death, the less willing they are to start doing something. The value of life is discounted by the fact that “I” in future is not “I” today. It is amazing – egoism kills people. Today I am enjoying life, and tomorrow I will sort everything out.
+
+I am not aiming at listing here all reasons why people do not agree with “it is better to be alive than to be dead”, but I will add one more.
+
+We tend to agree with the majority view for the questions that are not important to us due to conformity. Death ideology dominates in society – and it is easier to join it, especially since the task of eliminating death does not appear reachable.
+
+Actually, it is the only argument that has at least some rationale hint: it is highly unlikely that we will be able to substantially increase our lifespan during our own lives. As such, we must go and do what we must do, in order not to lose time. Talking about “being alive is better than dead” appears pointless and not useful.
+
+Let me give you some arguments, why our talk about the unacceptability of death makes sense.
+
+In places where people are more protected against death, people die less. That is the consequence of the death prevention mindset. More money is spent on security and medicine. Globally, death resistance leads to local victories.
+
+There is no physical law that prohibits extending life for an indefinite period. Parts of our body are replaceable, including brain cells. I am extremely excited by the numerous medical advancements in replacing organs and tissues. I can’t wait till they manage to attach a new body to a mammal’s head. I am not only waiting for, but also promoting this research field.
+
+Scientists have many times extended mammals’ lives. Moreover, in model animals we sometimes observe the multiplying effect of different ways to extend life.
+
+Actually, we have not tested even 1% of ideas for how to extend peoples’ lives. We cannot say: we put in a lot of effort and have not succeeded. What we can reliably say is: we have done almost nothing, and for some reason immortality has not immediately given up its secrets.
+
+In the above argument, that fight against death has little chances to be won and one can waste one’s time – the most important word is “to waste”. Let us discuss what is a waste of time and what is not. And what really makes sense in this world.
+
+Imagine that while you are reading this text, a nuclear war began and everybody died. It means I wasted my time writing this text and you wasted your time reading it. All I did in my life was in vain. And all you did was in vain. All people at all times lived, loved, worked absolutely in vain. Everything will lose its sense.
+
+Sense or meaning is a justification of existence. There will be nobody to justify.
+
+In this context only actions against a global catastrophe make sense, other actions are a harmful waste of time.
+
+But death of all people is equally terrible and “in vain”, regardless of the cause of death, e.g., old age. At the same time, when some people die of old age and others remain alive – their sense of existence is also alive. But! It is their sense of existence, not ours. A dead person takes their meaning of life to nowhere. It ceases to exist.
+
+Death nullifies all human achievements. No more senses or meanings. Therefore “in vain” is a guaranteed result if you are dead. Only the eternal makes sense. The rest is burning opportunities, even the small ones.
+
+It is important to underline once again, what is wrong with death. Everything is wrong in death; it deprives of all positives and produces nothing. Do people wish to die? Sometimes they do, first of all when a person suffers from pain. But it is a problem of pain, and does not mean that death is attractive per se. Many elderly people do not want to live. What is aging after all? It is slow death. People who experience problems with seeing, hearing, thinking, and cannot move as they used to – these people are practically dead. They do not allow themselves to want to become young, because it appears impossible.
+
+It also happens that problems and troubles make one’s life unbearable, and they see death as a rescue, a way out. In reality it is a bad way out, as there is no way back.
+
+Why should we extend life? What is valuable about our lives?
+
+Everything is valuable. Not only youth and beauty. The very fact of our existence is incredibly awesome. We can think and create new things.
+
+Imagine that you are a king of kings. The difference between you and a beggar is negligibly small if compared to the difference between an alive man and a dead man. And nowadays you can easily become a king of kings in the virtual space, or even become a boyfriend of Lana Del Ray.
+
+A few words about mortal ideology. This is a world picture where people give little importance to death. Death is a mega event in one’s life and a very negative event, too. Many people do not notice the fact that the flow of time leads them to the grave and do not think whether they can do something about it. That is what I call mortal ideology.
+
+From our transhumanists’ point of view, doing nothing against death is criminal, immoral, wild and insane. Finding excuses to death is equal to giving a gun to a terrorist killing people.
+
+Unfortunately, we cannot afford not to discuss the inhuman position of people who excuse death. An excuse lies even in the
+
+phrase “well, maybe there is something after death”. In reality, this should translate as “most likely there is nothing wrong with the fact that you will die”.
+
+“Unfortunately,” because when preparing themselves for death, these people prepare others as well. Since too few tickets are sold to the match against death, nobody paid for the scouts of a star team “life extension at all costs”.
+
+And what about overpopulation? This is no simple question that deserves a detailed answer, and I will not shrug it off by mentioning an option to populate outer space. There is a real conflict between altruism and egoism, and I will prove that altruism sometimes becomes inhuman.
+
+To be continued…
